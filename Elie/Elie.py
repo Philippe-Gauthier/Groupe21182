4a2087eb8220d361c6e2685befef142c1e068c8a
ElieThauvette <eliethauvette@gmail.com>
2026-01-30 13:29:11 -0500

    Update Elie.py

--- Elie/Elie.py
+++ Elie/Elie.py
@@ -1,21 +1,20 @@
 
 name = input("Entrez votre nom: ")
 
-porte = input("Bonjour, " + name + " faites votre chois entre 3 portes: gauche, droite ou centre?")
-#la console indique que la parentaise ne ferme pas
+porte = input("Bonjour, " + name + " faites votre chois entre 3 portes: gauche, droite ou centre? ")
 
 match porte:
 
     case "gauche":
-        print("Vous avez choisis la porte 1, celle de gauche.")
+        print("Vous avez choisis la porte 1, celle de gauche. ")
 
     case "droite":
-        print("Vous avez choisis la porte 2, celle de droite.")
+        print("Vous avez choisis la porte 2, celle de droite. ")
 
     case "centre":
-        print("Vous avez choisis la porte 3, celle au centre.")
+        print("Vous avez choisis la porte 3, celle au centre. ")
 
-equipement = input("Devans vous se trouvent trois objets, vous ne pouvez n'en prendre qu'un. La corde, la poêle ou le tournevis?")
+equipement = input("Devans vous se trouvent trois objets, vous ne pouvez n'en prendre qu'un. La corde, la poêle ou le tournevis? ")
 
 match equipement:
 
