311196e3c50d12a548499b1ce72dae14e9305d9d
rosemarie <daltonrosemarie@hotmail.com>
2026-01-30 15:29:10 -0500

    add more story

--- Rose/StrandedText.py
+++ Rose/StrandedText.py
@@ -2,34 +2,112 @@
 #options
 
 #p1
-initialText = "You and your friends are on a nice cruise during your summer break. Everything is smooth sailing, and all of you are having fun. You just woke up, and your friend, Theo, suggests you go to the brunch \nprovided by the crew."
+class p1 :
+    text = "You and your friends are on a nice cruise during your summer break. Everything is smooth sailing, and all of you are having fun. You just woke up, and your friend, Theo, suggests you go to the brunch provided by the crew."
 
-p1_option1 = "Yeah, I could eat."
-p1_option2 = "Nah, I'm gonna go shower."
-p1_option3 = "It's still morning, let me sleep!"
-p1_option4 = "Sorry, even if we're on vacation i have to stay fit, it's time for my run."
+    option1 = "Yeah, I could eat."
+    option2 = "Nah, I'm gonna go shower."
+    option3 = "It's still morning, let me sleep!"
+    option4 = "Sorry, even if we're on vacation i have to stay fit, it's time for my run."
 
 #p2
-p2_text = "You follow Theo to the brunch room, where your friends are already waiting for you.\nYou enter a luxurious and crowded room, with beautiful chandeliers and centerpieces your student \nincome could never afford. Sometimes you wish for a better life, but looking at your friends, you realize you are lucky to be so well surrounded. You join your friends and take a seat at the table. Right when \nyou bite into a deliciously buttered croissant, the cruise ship starts violently shaking. It feels like it \ncrashed into something. You look around trying to find answers, but there is not much you can do \nexcept hope for the best."
+class p2 :
+    text = "You follow Theo to the brunch room, where your friends are already waiting for you.\nYou enter a luxurious and crowded room, with beautiful chandeliers and centerpieces your student income could never afford. Sometimes you wish for a better life, but looking at your friends, you realize you are lucky to be so well surrounded. You join your friends and take a seat at the table. Right when \nyou bite into a deliciously buttered croissant, the cruise ship starts violently shaking. It feels like it crashed into something. You look around trying to find answers, but there is not much you can do except hope for the best."
 
-p2_option1 = "Hope for the best"
-p2_option2 = "Grab Theo's hand"
-p2_option3 = "Pray"
-p2_option4 = "Start crying"
+    option1 = "Hope for the best"
+    option2 = "Grab Theo's hand"
+    option3 = "Pray"
+    option4 = "Start crying"
 
 #p3
-p3_text = "You grab your towel and shampoo and walk to the shower room. You start the water and let the warmth engulf your cold skin. Nothing beats a nice warm shower to start your day. You wash your hair and \nscrub yourself clean, then enjoy a few more minutes under the hot water. Just as you're about to finish \ngetting dressed, the ship starts violently shaking. It feels like it crashed into something. You rapidly \nfinish putting your socks on, but there is not much you can do to help the situation."
+class p3 :
+    text = "You grab your towel and shampoo and walk to the shower room. You start the water and let the warmth engulf your cold skin. Nothing beats a nice warm shower to start your day. You wash your hair and scrub yourself clean, then enjoy a few more minutes under the hot water. Just as you're about to finish getting dressed, the ship starts violently shaking. It feels like it crashed into something. You rapidly finish putting your socks on, but there is not much you can do to help the situation."
 
-p3_option1 = "Hope for the best"
-p3_option2 = "Try to find your friends"
-p3_option3 = "Pray"
-p3_option4 = "Start crying"
+    option1 = "Hope for the best"
+    option2 = "Try to find your friends"
+    option3 = "Pray"
+    option4 = "Start crying"
 
 #p4
+class p4 :
+    text = "Like you said, you go back to sleep. You have beautiful dreams."
+
+    option1 = "Sleep"
+    option2 = "Sleep"
+    option3 = "Sleep"
+    option4 = "Sleep"
 
 #p5
+class p5 :
+    text = "You lazily get up, trying to motivate yourself to go run. You get dressed, put on your shoes, and grab your water bottle. Finally, you're out on the deck. You start running, but notice a strange shadow in the water. You tell yourself it must just be a weird reflection. The ocean itself is weird after all. Just as you start running again, the ship suddenly shakes violently, which throws you straight into the metal rails. Suddenly, your world goes dark."
+
+    option1 = "Try to wake up"
+    option2 = "Give in to the light"
+    option3 = "Stay unconscious"
+    option4 = "Have a vivid dream"
 
 #p6
 
-#p7
+class p6 :
+    text = "page 6"
 
+    option1 = "Sleep"
+    option2 = "Sleep"
+    option3 = "Sleep"
+    option4 = "Sleep"
+
+#p7
+class p7 :
+    text = "page 7"
+
+    option1 = "Sleep"
+    option2 = "Sleep"
+    option3 = "Sleep"
+    option4 = "Sleep"
+
+class p8 :
+    text = "page 8"
+
+    option1 = "Sleep"
+    option2 = "Sleep"
+    option3 = "Sleep"
+    option4 = "Sleep"
+
+class p9 :
+    text = "page 9"
+
+    option1 = "Sleep"
+    option2 = "Sleep"
+    option3 = "Sleep"
+    option4 = "Sleep"
+
+class p10 :
+    text = "page 10"
+
+    option1 = "Sleep"
+    option2 = "Sleep"
+    option3 = "Sleep"
+    option4 = "Sleep"
+
+class p11 :
+    text = "page 11"
+    option1 = "Sleep"
+    option2 = "Sleep"
+    option3 = "Sleep"
+    option4 = "Sleep"
+
+class p12 :
+    text = "page 12"
+
+    option1 = "Sleep"
+    option2 = "Sleep"
+    option3 = "Sleep"
+    option4 = "Sleep"
+
+class p13 :
+    text = "page 13"
+
+    option1 = "Sleep"
+    option2 = "Sleep"
+    option3 = "Sleep"
+    option4 = "Sleep"
--- Rose/project.py
+++ Rose/project.py
@@ -10,12 +10,12 @@ button c is pressed : page is 6
 
 so in button a, b, and c there are different options if the page was 2, and it sets the currPage to another unique id to continue this sequence
 
-(yes, the fractal tree is gonna be insane since there are 4 options. yes, i will ask if i can make a text file to ppick from to keep this code cleaner)
+(yes, the fractal tree is gonna be insane since there are 4 options.)
 
 if we want to change the story (inn future) simply add a class prefix or smt to be able to change aptly between stories
 
 """
-
+import textwrap as tw
 import tkinter as tk
 import StrandedText as st
 
@@ -27,31 +27,57 @@ currPage = 0
 
 
 
-newText = st.initialText
-option1 = st.p1_option1
-option2 = st.p1_option2
-option3 = st.p1_option3
-option4 = st.p1_option4
+newText = tw.fill(st.p1.text, 100)
+option1 = st.p1.option1
+option2 = st.p1.option2
+option3 = st.p1.option3
+option4 = st.p1.option4
+
+def setOptions(page) :
+    global option1
+    global option2
+    global option3
+    global option4
+    global newText
+    newText = page.text
+    option1 = page.option1
+    option2 = page.option2
+    option3 = page.option3
+    option4 = page.option4
+
+def updateText() :
+    text.config(state=tk.NORMAL)
+    text.delete('1.0', tk.END)
+    text.insert(tk.END, tw.fill(newText, 100))
+    text.config(state=tk.DISABLED)
+    buttonA.config(text=option1)
+    buttonB.config(text=option2)
+    buttonC.config(text=option3)
+    buttonD.config(text=option4)
 
     
 def button1Pressed() :
     global i
     global currPage
-    global newText
-    global option1
-    global option2
-    global option3
-    global option4
+ 
     i += 1
   
     if (i == 1) :
-        currPage = 2
-        
-        newText = st.p2_text
-        option1 = st.p2_option1
-        option2 = st.p2_option2
-        option3 = st.p2_option3
-        option4 = st.p2_option4
+        currPage = 2 
+        setOptions(st.p2)
+    elif (currPage == 2) :
+        currPage = 6
+        setOptions(st.p6)
+    elif (currPage == 3) :
+        currPage = 7
+        setOptions(st.p7)
+    elif (currPage == 4) :
+        currPage = 8
+        setOptions(st.p8)
+    elif (currPage == 5) :
+        currPage = 9
+        setOptions(st.p9)
+
         
         
     updateText()
@@ -59,83 +85,55 @@ def button1Pressed() :
 def button2Pressed() :
     global i
     global currPage
-    global newText
-    global option1
-    global option2
-    global option3
-    global option4
 
     i += 1
     
     if (i == 1) :
         currPage = 3
-        
-        newText = st.p3_text
-        option1 = st.p3_option1
-        option2 = st.p3_option2
-        option3 = st.p3_option3
-        option4 = st.p3_option4
+        setOptions(st.p3)
+    elif (currPage == 2) :
+        currPage = 10
+        setOptions(st.p10)
+    elif (currPage == 3) :
+        currPage = 11
+        setOptions(st.p11)
+    elif (currPage == 4) :
+        currPage = 12
+        setOptions(st.p12)
+    elif (currPage == 5) :
+        currPage = 13
+        setOptions(st.p13)
 
     updateText()
     
 def button3Pressed() :
     global i
     global currPage
-    global newText
-    global option1
-    global option2
-    global option3
-    global option4
 
     i += 1
     
     if (i == 1) :
         currPage = 4
-        print("mlem")
+        setOptions(st.p4)   
         
-        
-    text.config(state=tk.NORMAL)
-    text.delete('1.0', tk.END)
-    text.insert(tk.END, newText)
-    text.config(state=tk.DISABLED)
-    buttonA.config(text=option1)
-    buttonB.config(text=option2)
-    buttonC.config(text=option3)
-    buttonD.config(text=option4)
+    updateText()
     
 def button4Pressed() :
     global i
     global currPage
-    global newText
-    global option1
-    global option2
-    global option3
-    global option4
+
     i += 1
     
     if (i == 1) :
         currPage = 5
-        print("meep")
+        setOptions(st.p5)
         
-        
-    text.config(state=tk.NORMAL)
-    text.delete('1.0', tk.END)
-    text.insert(tk.END, newText)
-    text.config(state=tk.DISABLED)
-    buttonA.config(text=option1)
-    buttonB.config(text=option2)
-    buttonC.config(text=option3)
-    buttonD.config(text=option4)
+    updateText()
 
-def updateText() :
-    text.config(state=tk.NORMAL)
-    text.delete('1.0', tk.END)
-    text.insert(tk.END, newText)
-    text.config(state=tk.DISABLED)
-    buttonA.config(text=option1)
-    buttonB.config(text=option2)
-    buttonC.config(text=option3)
-    buttonD.config(text=option4)
+
+
+
+
     
 window = tk.Tk()
 
@@ -149,8 +147,6 @@ window.geometry("980x600")
 window.configure(bg="grey")
 
 
-# initial_text = "This is a multi-line text area positioned \nwith the grid manager.\nIt will expand with the window."
-# text.insert(tk.END, initial_text)
 text.grid(row=0, column=0, columnspan=2, sticky="NSEW", padx=5, pady=5)
 window.grid_rowconfigure(index=0, weight=0)
 window.grid_columnconfigure(index=0, weight=1)
