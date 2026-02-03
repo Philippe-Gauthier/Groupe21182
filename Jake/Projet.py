a587234d928754102ca1f82ade6020e88757bcb0
Zeruel-Ara <jacobchagnon506@gmail.com>
2026-02-03 14:09:37 -0500

    Update Projet.py

--- Jake/Projet.py
+++ Jake/Projet.py
@@ -11,4 +11,10 @@ Fichier principal projet livre interactif
 """
 
 print("Un mur d'arbre se dresse devant toi. Les épines de conifères s'étendent jusqu'à l'horizon, et ce, de chaque côté.")
-print("Une affiche à l'orée de la forêt te prévient que tu es sur le point de passer sur un terrain privé, mais un objet brillant tente de t'attirer vers l'intérieur. Que fais-tu?")
+print("Une affiche à l'orée de la forêt te prévient que tu es sur le point de passer sur un terrain privé, mais une voix tente de t'attirer vers l'intérieur. Que fais-tu?")
+choice = input("Tu t'avances vers la forêt (F) ou tu t`éloignes (E)")
+if choice == "E":
+    print("Ton aventure se termine avant même d'avoir débuté. Tu rates ta chance. C'est fini.")
+
+if choice == "F":
+    print("")
