13c317f275dc6b6af404a31a21a64f1ce152d6c7
Zeruel-Ara <jacobchagnon506@gmail.com>
2026-02-10 14:51:30 -0500

    Update Projet.py

--- Jake/Projet.py
+++ Jake/Projet.py
@@ -15,24 +15,32 @@ Sortie : option choisie (texte)
 But : Demander au lecteur de faire un choix
 """
 
+
+
 def choisir_option(choix_1, choix_2):
     print("************************************")
     print(f"1: {choix_1}")
     print(f"2: {choix_2}")
-    decision = int(input)
+
+    decision = (input)
     return  decision
 
 
 # Début du jeu
-
-choice = input("Tu t'avances vers la forêt (1) ou tu t`éloignes (2)")
-choix_1 = print("Les feuilles craquent sous tes pieds. La voix se fait rassurante; comme un petit frère qui essaie d'avoir ton attention. ")
+choix_1 = print()
 choix_2 = print("L'aventure se termine avant d'avoir commencé.")
+choisir_option(choix_1, choix_2)
 
 
-print("Un mur d'arbre se dresse devant toi. Les épines de conifères s'étendent jusqu'à l'horizon, et ce, de chaque côté.")
+
+print("Un mur d'arbres se dresse devant toi. Les épines de conifères s'étendent jusqu'à l'horizon, et ce, de chaque côté.")
 print("Une affiche à l'orée de la forêt te prévient que tu es sur le point de passer sur un terrain privé, mais une voix tente de t'attirer vers l'intérieur.")
 print("Que fais-tu?")
+
 choisir_option(choix_1, choix_2)
-# premier choix
+choice = input("Tu t'avances vers la forêt (1) ou tu t`éloignes (2)")
+if choice == 2: 
+    print("L'aventure se termine avant d'avoir commencé.")
+if choice == 1:
+    print("Les feuilles craquent sous tes pieds. La voix se fait rassurante; comme un petit frère qui essaie d'avoir ton attention. ")
 
