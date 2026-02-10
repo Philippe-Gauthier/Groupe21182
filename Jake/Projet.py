91185115536ecae947fbce8ba44e19c6b34a5bc3
Zeruel-Ara <jacobchagnon506@gmail.com>
2026-02-10 14:21:57 -0500

    Update Projet.py

--- Jake/Projet.py
+++ Jake/Projet.py
@@ -9,15 +9,30 @@ Fichier principal projet livre interactif
 
 
 """
+""" 
+Entree : les choix (texte)
+Sortie : option choisie (texte)
+But : Demander au lecteur de faire un choix
+"""
+
+def choisir_option(choix_1, choix_2):
+    print("************************************")
+    print(f"1: {choix_1}")
+    print(f"2: {choix_2}")
+    decision = int(input("Quelle option?"))
+    return  decision
+
+
 # Début du jeu
+
+choice = input("Tu t'avances vers la forêt (1) ou tu t`éloignes (2)")
+choix_1 = print("Les feuilles craquent sous tes pieds. La voix se fait rassurante; comme un petit frère qui essaie d'avoir ton attention. ")
+choix_2 = print("L'aventure se termine avant d'avoir commencé.")
+
+
 print("Un mur d'arbre se dresse devant toi. Les épines de conifères s'étendent jusqu'à l'horizon, et ce, de chaque côté.")
-print("Une affiche à l'orée de la forêt te prévient que tu es sur le point de passer sur un terrain privé, mais une voix tente de t'attirer vers l'intérieur. Que fais-tu?")
+print("Une affiche à l'orée de la forêt te prévient que tu es sur le point de passer sur un terrain privé, mais une voix tente de t'attirer vers l'intérieur.")
+print("Que fais-tu?")
+choisir_option(choix_1, choix_2)
 # premier choix
-# mauvais choix
-choice = input("Tu t'avances vers la forêt (F) ou tu t`éloignes (E)")
-if choice == "E":
-    print("Ton aventure se termine avant même d'avoir débuté. L'histoire oublie ton nom.")
-    print("GAME OVER")
-# bon choix
-if choice == "F":
-    print("")
+
