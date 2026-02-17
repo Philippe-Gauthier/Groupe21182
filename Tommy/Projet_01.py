555b6b739a41eb05b06d59621817cff86585e22c
tommybrunelle29 <tommy.brunelle29@gmail.com>
2026-02-17 14:42:24 -0500

    Update Projet_01.py

--- Tommy/Projet_01.py
+++ Tommy/Projet_01.py
@@ -9,6 +9,19 @@ page_03 = "Une fois arrivé, le chef te dévisage instantanémment. Il te reniff
 page_04 = "Il te faut des chaudières, tu cherches un peu et entre dans une chambre froide où il y a plein plein plein de chaudières.\n\nQue prends-tu? "
 page_05 = "Le cuisinier te sourit. «Oublie les patates big, Marco s'en occupe. Aide moi à couper des légumes pour ce soir.»"
 page_06 = "Le chef te voit se promener avec une cannette de peinture blanche, se fâche et te renvoi sur le champ.\n\nGame Over"
+page_07 = ""
+page_08 = ""
+page_09 = ""
+page_10 = ""
+page_11 = ""
+page_12 = ""
+page_13 = ""
+page_14 = ""
+page_15 = ""
+page_16 = ""
+page_17 = ""
+page_18 = ""
+
 choix_intro_01 = "Tu prends un bon grand respir et te dirige au travail, ça va passer."
 choix_intro_02 = "Tu décides de prendre 2-3 shooters pour la confiance, il est midi quelque part comme on dit"
 choix_02_01 = "Tu décides d'aller remplir une chaudière d'eau bouillante salée et une chaudière d'eau glacée pour les patates."
@@ -16,8 +29,15 @@ choix_02_02 = "Tu restes immobile, paniqué. Tu vois un cuisinier au loin qui te
 choix_02_03 = "Tu fouilles partout à la recherche de peinture blanche."
 choix_04_01 = "Tu vois deux chaudières remplies d'eau. Tu décides de sauver du temps pour mieux paraître. Tu pars avec."
 choix_04_02 = "Tu prends les deux chaudières vides du bord et retourne en cuisine."
-choix_05_01 = ""
-choix_05_02 = ""
+choix_05_01 = "Tu décides d'aiguiser ton couteau avant de couper quoi que ce soit."
+choix_05_02 = "Tu prends un couteau et fixe la carotte devant toi."
+choix_05_03 = "Tu lui avoue ne rien savoir et que tu as besoin d'aide pour ne pas que ça paraisse."
+choix_08_01 = ""
+choix_08_02 = ""
+choix_10_01 = ""
+choix_10_02 = ""
+choix_11_01 = ""
+choix_11_02 = ""
 
 
 def choisir_deux_options(choix1, choix2):
@@ -79,32 +99,51 @@ def choisir_trois_options(choix1, choix2, choix3):
 
 print("   ")
 print(intro)
-decision_01 = choisir_deux_options(choix_intro_01, choix_intro_02)
+decision_intro = choisir_deux_options(choix_intro_01, choix_intro_02)
 
-if decision_01 == 2:
+if decision_intro == 2:
     print("   ")
     print(page_02)
     decision_02 = choisir_trois_options(choix_02_01, choix_02_02, choix_02_03)
 
-if decision_01 == 1:
+    if decision_02 == 1:
+        print("   ")
+        print(page_04)
+        decision_04 = choisir_deux_options(choix_04_01, choix_04_02)
+
+        if decision_04 == 1:
+            print("   ")
+            print(page_07)
+
+        elif decision_04 == 2:
+            print("   ")
+            print(page_08)
+
+    elif decision_02 == 2:
+        print("   ")
+        print(page_05)
+        decision_05 = choisir_trois_options(choix_05_01, choix_05_02, choix_05_03)
+
+        if decision_05 == 1:
+            print("   ")
+            print(page_09)
+
+        elif decision_05 == 2:
+            print("   ")
+            print(page_10)
+
+        elif decision_05 == 3:
+            print("   ")
+            print(page_11)
+
+    elif decision_02 == 3:
+        print("   ")
+        print(page_06)
+
+elif decision_intro == 1:
     print("   ")
     print(page_03)
 
-
-
-if decision_02 == 1:
-    print("   ")
-    print(page_04)
-    decision_03 = choisir_deux_options(choix_04_01, choix_04_02)
-
-if decision_02 == 2:
-    print("   ")
-    print(page_05)
-    decision_04 = choisir_deux_options(choix_05_01, choix_05_02)
-
-if decision_02 == 3:
-    print("   ")
-    print(page_06)
 
 
     
