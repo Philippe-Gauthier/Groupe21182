e82d1845ad5bca9b872e29ddd6dc15570e1cd8f3
tommybrunelle29 <tommy.brunelle29@gmail.com>
2026-02-19 12:50:33 -0500

    Update Projet_01.py

--- Tommy/Projet_01.py
+++ Tommy/Projet_01.py
@@ -3,26 +3,56 @@ Tommy Brunelle
 Fichier principal du projet de livre interactif
 
 """
-absence_choix = " "
-intro = "C'est le grand jour, tes mensonges ont porté fruit.\nTu commences ton premier quart de travail en tant que chef cuisinier dans 1h.\nTu es remplis de remords face aux mensonges liés à cette embauche, mais il faut ce qu'il faut pour arriver hein?\nTu dois te préparer à partir, mais tu es terriblement stressé.\n\nQue fais tu?"
-page_02 = "Tu arrives en cuisine et tout semble normal, personne ne mentionne l'odeur d'alcool. Le chef arrive et te salue. «On est dans le rush déjà pour la préparation, va blanchir des patates.»\n\nQue choisis-tu de faire?"
+# Pages du livres/Mises en situation.
+intro = "C'est le grand jour, tes mensonges ont porté fruit.\nTu commences ton premier quart de travail en tant que chef cuisinier dans 1h.\nTu es remplis de remords face aux mensonges liés à cette embauche, mais il faut ce qu'il faut pour arriver hein?\nTu dois te préparer à partir, mais tu es terriblement stressé."
+page_02 = "Tu arrives en cuisine et tout semble normal, personne ne mentionne l'odeur d'alcool. Le chef arrive et te salue. «On est dans le rush déjà pour la préparation, va blanchir des patates.»"
 page_03 = "Une fois arrivé, le chef te dévisage instantanémment. Il te reniffle. «T'es clairement ajeun...T'es pas un vrai cook!» Il te crie dessus et te force à quitter les lieux.\n\nGame Over\n"
-page_04 = "Il te faut des chaudières, tu cherches un peu et entre dans une chambre froide où il y a plein plein plein de chaudières.\n\nQue prends-tu? "
+page_04 = "Il te faut des chaudières, tu cherches un peu et entre dans une chambre froide où il y a plein plein plein de chaudières."
 page_05 = "Le cuisinier te sourit. «Oublie les patates big, Marco s'en occupe. Aide moi à couper des légumes pour ce soir.»"
 page_06 = "Le chef te voit se promener avec une cannette de peinture blanche, se fâche et te renvoi sur le champ.\n\nGame Over\n"
-page_07 = "Bravo imbécile. Après avoir coupé plusieurs patates, le chef passe proche et voit tes chaudières.\nElles sont pour faire tremper les têtes de moppe, tu perds ton emploi.\n\nGame Over"
-page_08 = "Bien joué! Jusqu'à présent personne ne soupçonne ton manque extrême de connaissances culinaires. T'as réussi à blanchir une chaudière de patates.\n\nMaintenant quoi?"
-page_09 = "Tu ne sais pas aiguiser un couteau. Tu te coupe violemment la main et tu saignes dans tous les légumes déjà préparés.\nOn te renvoi et tu dois toi-même appeler l'ambulance."
-page_10 = "L'angoisse s'empare de toi, tu te mets à suer à profusion. Tu regardes à nouveau le cuisinier, il a un tattoo de larme sous l'oeil, tu ne te sens pas très bien.\nQue fais-tu?"
+page_07 = "Bravo imbécile. Après avoir coupé plusieurs patates, le chef passe proche et voit tes chaudières.\nElles sont pour faire tremper les têtes de moppe, tu perds ton emploi.\n\nGame Over\n"
+page_08 = "Bien joué! Jusqu'à présent personne ne soupçonne ton manque extrême de connaissances culinaires. T'as réussi à blanchir une chaudière de patates."
+page_09 = "Tu ne sais pas aiguiser un couteau. Tu te coupe violemment la main et tu saignes dans tous les légumes déjà préparés.\nOn te renvoi et tu dois toi-même appeler l'ambulance.\n\n Game Over\n"
+page_10 = "L'angoisse s'empare de toi, tu te mets à suer à profusion. Tu regardes à nouveau le cuisinier, il a un tattoo de larme sous l'oeil, tu ne te sens pas très bien."
 page_11 = "La chance te sourit, il t'avoue lui aussi avoir menti sur son CV. «T'as juste à donner des ordres et les gens pensent que tu sais ce que tu fais.»"
-page_12 = ""
-page_13 = ""
-page_14 = ""
-page_15 = ""
-page_16 = ""
-page_17 = ""
-page_18 = ""
+page_12 = "Le chef passe près de toi et voit que tu ne fais rien. Il te demande avec un ton fâché d'aller chercher des culs de poule pour continuer la préparation."
+page_13 = "Tu viens de te tirer dans le pied. Une pause? En cuisine? Je savais pas que les princesses se cherchaient de l'ouvrage. Le chef te renvoie.\n\nGame Over\n"
+page_14 = "Le chef te regardes avec dégoût. «Tu m'écoeures, remplis de jugement. On est en 2026, ce tattoo de larme représente sa sensibilité. Sors de ma cuisine.» \n\nGame Over\n"
+page_15 = "Il te regarde confus. «Mon premier quoi?» Il essuie son visage. Oh oh, c'etait juste de la saleté."
+page_16 = "Un des cuisinier sursaute , te regarde et se dépêche à aller blanchir des patates. «Wow ca marche pour vrai!» Tu prends tes aises."
+page_17 = "«Est-ce que tu me donnes le même ordre que je viens juste de te donner? Fuck off man.» Il va voir le chef et lui apprend que tu ne sais rien. Il te renvoi.\n\nGame Over\n"
+page_18 = "Le cuisinier te regarde. «Non.»"
+page_19 = "Le chef a un air contrarié. «Ça te fais rire la cuisine? Sais-tu c'est quoi un cul de poule?»"
+page_20 = "Le chef te regarde confus. «Ok, t'as les oeufs...t'as pas pris les culs de poule pour les mettre dedans?»"
+page_21 = "Par miracle tu es accidentellement tombé sur le bon item, fiou. Le chef enchaîne. «Parfait!, maintenant prend une langue de chat, on en a pas beaucoup, t'en auras besoin tantôt.» "
+page_22 = "Il le prend comme une insulte, comme si tu le narguais. «Eille le nouveau! C'est quoi ton esti de problème?"
+page_23 = "Il pense que tu parles de son tattoo sur la main. Il te sourit et réponds que non, il en a plein. «Tu veux les voir?»"
+page_24 = "Tu claquais des doigts tellement fort, le chef t'as entendu et t'as vu faire. Il se rend compte que tu fais n'importe quoi. Tu perds ton emploi.\n\nGame Over\n"
+page_25 = "Un cuisinier te réponds: «T'as juste à te regarder dans le miroir big!» Il rit bien trop fort, tout le monde se retourne."
+page_26 = "Tu es surpris et stupéfait de voir à quel point énormément de cuisiniers ont des cigarettes sur eux.\nTu es encore plus impressionné de voir à quel point personne ne s'est posé de question en te lancant leur paquet."
+page_27 = "Il s'approche et te pousse. Tu tombes contre le four et te brûle la main. Tu cries, tu as mal, personne ne t'aides. Tu quittes sans avertir. Le chef te texte plus tard pour te dire qu'il n'aime pas les fuyards. Tu es renvoyé.\n\nGame Over\n"
+page_28 = "Il s'approche avec un air bête. «Si je te donne 5$, est-ce que t'arrêtes de me parler?» "
+page_29 = "Il ne dit pas un mot. Il sait que tu ne sais pas. Il ne fait que pointer la sortie.\n\nGame Over\n"
+page_30 = "Tu n'as jamais vu un visage aussi rouge de colère. Il crie et t'obliges à quitter.\n\nGame Over\n"
+page_31 = "Étrangement il te crois et quittes. Tu passes un très beau fin de quart de travail.\n\nVictoire\n"
+page_32 = "Il te réponds: «Je suis pas prof, je suis chef. Va-t'en.»\n\nGame Over\n"
+page_33 = "Le chef te regardes intrigué. «Non... Peut-être que vous appeliez ça des maryses dans ton cours?»"
+page_34 = "Il te réponds calmement: «Ok j'en ai vu assez, va-t'en.»\n\nGame Over\n"
+page_35 = "Sa question n'en était pas une vraie. Cet homme a un problème de tempérament.\nIl s'avance vers toi et il t'enligne une droite au visage. Le chef vous renvoi les deux sur le champs.\n\nGame Over\n"
+page_36 = "Tu te réveilles à l'hôpital.\n\nGame Over\n"
+page_37 = "Il le prend super mal. Malheureusement pour toi, c'est le fils du chef. Il va voir son père et tu perds ta job.\n\nGame Over\n"
+page_38 = "Il enlève son chandail et est maintenant torse nu dans la cuisine. Un inspecteur passe au même moment et oblige la cuisine à fermer.\nTechniquement, ce n'est pas un renvoi.\n\nVictoire\n"
+page_39 = "Personne ne rit. Cette blague est misogyne, irrespectueuse et de trop. Tu es étrange, lourd et inconfortable à côtoyer. On te demande de partir.\n\nGame Over\n"
+page_40 = "Étrangement personne ne rentre dans ce frigo de tout le reste de ton quart de travail et personne ne remarque ton absence. Tu sors à la fin de ton shift. Félicitations.\n\nVictoire\n"
+page_41 = "Come on, la cigarette c'est vilain. Tu ne perds pas ton emploi, mais tu perds au jeu de la vie.\n\nGame Over\n"
+page_42 = "Toute la cuisine fume, tu deviens automatiquement la honte du restaurant. Plus personne ne te parle et t'aide, le chef se rend vite compte que tu ne sais pas quoi faire. Il te demande de quitter.\n\nGame Over\n"
+page_43 = "Il prend ton 50$ et quitte. Tu passes le restant de ton quart de travail aux aguets, mais tout se passe à merveilles. Félicitations.\n\nVictoire\n"
+page_44 = "Il te donne 5$ et te laisse tranquille. Le chef vient te voir et t'annonce que la préparation est maintenant terminée. Félicitation, tu as terminé la premiere partie de ton premier shift coupé.\n\nVictoire\n"
+page_45 = "Le chef rit et quitte. Il pense que tu le niaises à 100%. Il se dit déjà qu'il va beaucoup t'apprécier.\nBravo, ton premier quart de travail est déjà terminé. Tu quittes la tête haute, mais tjrs aussi inutile.\n\nVictoire\n"
+page_46 = "Le chef n'a pas l'air d'apprécier. Il soupir fort et te dis de laisser ton tablier à l'entrée.\n\nGame Over\n"
 
+# Options disponibles pour chaque page.
+# Nomenclature: choix_(# de page)_(choix # x)
 choix_intro_01 = "Tu prends un bon grand respir et te dirige au travail, ça va passer."
 choix_intro_02 = "Tu décides de prendre 2-3 shooters pour la confiance, il est midi quelque part comme on dit"
 choix_02_01 = "Tu décides d'aller remplir une chaudière d'eau bouillante salée et une chaudière d'eau glacée pour les patates."
@@ -40,8 +70,11 @@ choix_10_02 = ""
 choix_11_01 = ""
 choix_11_02 = ""
 
+# Variable pour la fonction de choix.
+absence_choix = " "
 
-def choisir_trois_options(choix1, choix2, choix3):
+# Fonction pour choix
+def choisir_options(choix1, choix2, choix3):
       
     """
     Entrées: Deux ou Trois choix (texte)
@@ -57,6 +90,7 @@ def choisir_trois_options(choix1, choix2, choix3):
           print(f"1: {choix1}")
           print(f"2: {choix2}")
           print("   ")
+          print("Que fais-tu?")
           decision = (input("Choix: "))
 
           if decision == "1" or decision == "2" or decision == "3":
@@ -75,6 +109,7 @@ def choisir_trois_options(choix1, choix2, choix3):
           print(f"2: {choix2}")
           print(f"3: {choix3}")
           print("   ")
+          print("Que fais-tu?")
           decision = (input("Choix: "))
 
           if decision == "1" or decision == "2" or decision == "3":
@@ -89,17 +124,17 @@ def choisir_trois_options(choix1, choix2, choix3):
 
 print("   ")
 print(intro)
-decision_intro = choisir_trois_options(choix_intro_01, choix_intro_02, absence_choix)
+decision_intro = choisir_options(choix_intro_01, choix_intro_02, absence_choix)
 
 if decision_intro == 2:
     print("   ")
     print(page_02)
-    decision_02 = choisir_trois_options(choix_02_01, choix_02_02, choix_02_03)
+    decision_02 = choisir_options(choix_02_01, choix_02_02, choix_02_03)
 
     if decision_02 == 1:
         print("   ")
         print(page_04)
-        decision_04 = choisir_trois_options(choix_04_01, choix_04_02, absence_choix)
+        decision_04 = choisir_options(choix_04_01, choix_04_02, absence_choix)
 
         if decision_04 == 1:
             print("   ")
@@ -112,7 +147,7 @@ if decision_intro == 2:
     elif decision_02 == 2:
         print("   ")
         print(page_05)
-        decision_05 = choisir_trois_options(choix_05_01, choix_05_02, choix_05_03)
+        decision_05 = choisir_options(choix_05_01, choix_05_02, choix_05_03)
 
         if decision_05 == 1:
             print("   ")
