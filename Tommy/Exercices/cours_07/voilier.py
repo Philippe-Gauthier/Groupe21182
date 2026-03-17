c655bea9badf41213fb3bb45aa0b749a33406911
tommybrunelle29 <tommy.brunelle29@gmail.com>
2026-03-17 13:36:33 -0400

    exercices

---
+++ Tommy/Exercices/cours_07/voilier.py
@@ -0,0 +1,14 @@
+dimension = (30.5, 9.2)
+print(dimension[0])
+print(dimension[1])
+
+inventaire = []
+
+inventaire.append("Gilets")
+inventaire.append("Rames")
+inventaire.append("Ancre")
+
+inventaire.remove("Rames")
+
+for i in inventaire:
+    print(i)
