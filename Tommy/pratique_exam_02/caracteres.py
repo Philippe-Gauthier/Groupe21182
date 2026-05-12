19f4b62bdf5f2dee7ca6fe380160d4b485108d18
tommybrunelle29 <tommy.brunelle29@gmail.com>
2026-05-12 14:02:00 -0400

    pratique exam_02 sans fonctions

---
+++ Tommy/pratique_exam_02/caracteres.py
@@ -0,0 +1,3 @@
+# La liste des caracteres autorises par l'auditeur
+
+caracteres_autorises = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",".",","," ","\n"]
